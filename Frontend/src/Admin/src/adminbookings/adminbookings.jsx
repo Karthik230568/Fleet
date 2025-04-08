5f9de615b0076f8907b68e7c88c1c370fae5a99a
iamshashi-10 <shashi123bhidodiya@gmail.com>
2025-04-08 06:51:14 +0530

    adminBooking

--- Frontend/src/Admin/src/adminbookings/adminbookings.jsx
+++ Frontend/src/Admin/src/adminbookings/adminbookings.jsx
@@ -2,35 +2,15 @@ import React from "react";
 import "./adminbookings.css";
 import AdminbookingList from "./bookinglist";
 import Calendar from "./calendar";
-import Adminbookingssidebar from "./sidebar";
-//import { useNavigate } from "react-router-dom";
+// import Adminbookingssidebar from "./sidebar";
+
+// importing adminBookingStore
+import useAdminBookingStore from "../../../../store/adminBookingStore";
+
 function Adminbookingspage() {
-  const bookings = [
-  
-    {
-      id: 2,
-      vehicle: "KTM RC 360",
-      pickupDate: "04/02/2025 17:00",
-      duration: "2 days 21 hours",
-      returnDate: "06/02/2025 14:00",
-      totalAmount: "5000 INR",
-      user: "Sai@gmail.com",
-      driverName: "Sai",
-      vehicleId: "1234567890",
-    },
-    {
-      id: 1,
-      vehicle: "Hyundai i30",
-      pickupDate: "31/01/2025 11:00",
-      duration: "5 days 3 hours",
-    returnDate: "05/02/2025 14:00",
-      totalAmount: "20000 INR",
-      user: "Sai@gmail.com",
-      driverName: "Sai",
-      vehicleId: "1234567890",
-    }   
-    
-  ];
+
+  // Get bookings from the adminBookingStore
+  const bookings = useAdminBookingStore((state) => state.bookings);
 
   return (
     <div className="app">
@@ -38,10 +18,41 @@ function Adminbookingspage() {
         <Calendar />
       </div>
       <div className="main-content">
-       <AdminbookingList bookings={bookings} />
+        <AdminbookingList bookings={bookings} />
       </div>
     </div>
   );
 }
 
 export default Adminbookingspage;
+
+
+
+
+// previously hardcoded bookings data
+  // const bookings = [
+  
+  //   {
+  //     id: 2,
+  //     vehicle: "KTM RC 360",
+  //     pickupDate: "04/02/2025 17:00",
+  //     duration: "2 days 21 hours",
+  //     returnDate: "06/02/2025 14:00",
+  //     totalAmount: "5000 INR",
+  //     user: "Sai@gmail.com",
+  //     driverName: "Sai",
+  //     vehicleId: "1234567890",
+  //   },
+  //   {
+  //     id: 1,
+  //     vehicle: "Hyundai i30",
+  //     pickupDate: "31/01/2025 11:00",
+  //     duration: "5 days 3 hours",
+  //   returnDate: "05/02/2025 14:00",
+  //     totalAmount: "20000 INR",
+  //     user: "Sai@gmail.com",
+  //     driverName: "Sai",
+  //     vehicleId: "1234567890",
+  //   }   
+    
+  // ];
--- Frontend/src/Admin/src/adminbookings/bookinglist.jsx
+++ Frontend/src/Admin/src/adminbookings/bookinglist.jsx
@@ -4,18 +4,19 @@ import "./bookinglist.css";
 function AdminbookingList({ bookings }) {
   return (
     <div className="booking-list">
+      <h1>{console.log(bookings.length)}</h1>
       {bookings.map((booking) => (
-        <div key={booking.id} className="booking-card">
+        <div key={booking.bookingId} className="booking-card">
           <div className="vehicle-info">
-            <span className="vehicle-name">{booking.vehicle}</span>
-            <span className="user-name">{booking.user}</span>
+            <span className="vehicle-name">{booking.vehicleName}</span>
+            <span className="user-name">{booking.userName}</span>
           </div>
           <div className="booking-details">
             {booking.pickupDate} | {booking.duration} | {booking.returnDate}
           </div>
           
-          <div className="driver-name">{booking.driverName}</div>
-          <div className="vehicle-id">{booking.vehicleId}</div>
+          <div className="driver-name">{booking.driver.name}</div>
+          <div className="vehicle-id">{booking.vehicle.vehicleId}</div>
           <div className="price">{booking.totalAmount}</div>
         </div>
       ))}
