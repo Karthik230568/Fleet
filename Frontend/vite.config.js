6a2c70ccbf0fbdef09191c844ee8c394d3e0e400
saiprabhav31 <addulasaiprabhav31@gmail.com>
2025-04-18 17:14:43 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:5000",
+        target: "https://fleet-idrc.onrender.com",
         changeOrigin: true,
         secure: false,
         ws: true,
